dbefdcb426bca4048e728fd5bc238018f162bcc4
Tunwashe Daniel <tunwashed@gmail.com>
2023-09-09 00:20:20 +0100

    Made some changes

--- runserver.py
+++ runserver.py
@@ -2,8 +2,8 @@ from api import create_app
 from api.config.config import config_dict
 
 
-app=create_app(config_dict=['prod'])
+app=create_app()
 
 
 if __name__ =="__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=5000)
